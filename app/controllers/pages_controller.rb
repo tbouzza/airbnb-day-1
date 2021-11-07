8fbba64c4cd2688d304edee54ef7243622ad6f01
Tarig Bouazzati <tbouzza@gmail.com>
2021-11-07 10:11:58 +0400

    setup devise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
+    # Display a message to the signed in user
+    # 1. Find the user -> I have to have her ID
+    # DEVISE Helper!
+    if user_signed_in?
+      @user_mail = current_user.email
+    end
+    # raise
   end
 end
